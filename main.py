e5f714b309d52f73317038cb28bd35d311af8521
FedorPinchuk <fpinchuk@fpinchuk.ru>
2025-06-10 20:51:07 +0500

    D: added all stats functions

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from stats import *
 
 def main():
-    print(f"{count_book_words("books/frankenstein.txt")} words found in the document")
-    print(count_book_symbols("books/frankenstein.txt"))
+    non_alpha_removed_dict = dict_non_alpha_removed(count_book_symbols("books/frankenstein.txt"))
+    list_of_dict = dict_to_dictlist(non_alpha_removed_dict)
+    list_of_dict.sort(reverse=True, key=sort_on)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {count_book_words("books/frankenstein.txt")} total words")
+    print("--------- Character Count -------")
+    for dict in list_of_dict:
+        print(f"{dict["char"]}: {dict["num"]}")
 
 main()
--- stats.py
+++ stats.py
@@ -15,7 +15,33 @@ def count_book_symbols(path_to_book):
     return book_stats
 
 def count_book_words(path_to_book):
+    
     with open(path_to_book) as f:
         book_text = f.read()
 
     return len(book_text.split())
+
+def dict_non_alpha_removed(dict_to_sort):
+    
+    sorted_dict = {}
+
+    for key in dict_to_sort:
+        if key.isalpha():
+            sorted_dict[key] = dict_to_sort[key]
+    
+    return sorted_dict
+
+def dict_to_dictlist(dict_to_modify):
+
+    dictlist = []
+
+    for key in dict_to_modify:
+        modified_dict = {}
+        modified_dict["char"] = key
+        modified_dict["num"] = dict_to_modify[key]
+        dictlist.append(modified_dict)
+
+    return dictlist
+
+def sort_on(dict):
+    return dict["num"]
