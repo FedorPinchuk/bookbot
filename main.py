891767899d1e590701a4c7de41ac3c464cf61b8f
FedorPinchuk <fpinchuk@fpinchuk.ru>
2025-06-11 21:37:48 +0500

    E: added sys module, bookbot finished

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from stats import *
+import sys
 
 def main():
-    non_alpha_removed_dict = dict_non_alpha_removed(count_book_symbols("books/frankenstein.txt"))
+
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    non_alpha_removed_dict = dict_non_alpha_removed(count_book_symbols(sys.argv[1]))
     list_of_dict = dict_to_dictlist(non_alpha_removed_dict)
     list_of_dict.sort(reverse=True, key=sort_on)
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {sys.argv[1]}...")
     print("----------- Word Count ----------")
-    print(f"Found {count_book_words("books/frankenstein.txt")} total words")
+    print(f"Found {count_book_words(sys.argv[1])} total words")
     print("--------- Character Count -------")
     for dict in list_of_dict:
         print(f"{dict["char"]}: {dict["num"]}")
